db5bb123827d612773e10896f3cb9da2a8159ac9
Trenton <050trenton@gmail.com>
2022-09-19 08:43:50 -0400

    starting on the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1 className='title'>Buy Best</h1>
+      <div className='header'>
+        <h1 className="title">Buy Best</h1>
+        <div className="bar">
+          <button className="departments">All departments</button>
+
+          <button className="phone-number">618-465-3464</button>
+        </div>
+      </div>
     </div>
   );
 }
